3bddbcaf234f4b5bc121d8189889b6f901dd9849
TegarUtomo <omoturaget2@gmail.com>
2018-07-25 07:47:31 +0700

    tambah Model

---
+++ src/main/java/com/tegareng/perpustakaan/model/UserModel.java
@@ -0,0 +1,12 @@
+package com.tegareng.perpustakaan.model;
+
+import java.io.Serializable;
+
+public class UserModel implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4766531921977686853L;
+
+}
